335a1647a6fbcd11a3a72a911f5644a8a6661e2a
Kumar, Himanshu (TCS) <hkumm@allstate.com>
2016-02-02 12:18:40 -0600

    Initial Commit

---
+++ Acronym/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Acronym
+//
+//  Created by Kumar, Himanshu (TCS) on 2/2/16.
+//  Copyright © 2016 Kumar, Himanshu (TCS). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Acronym/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Acronym
+//
+//  Created by Kumar, Himanshu (TCS) on 2/2/16.
+//  Copyright © 2016 Kumar, Himanshu (TCS). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
